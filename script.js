9caca2b4d56d0a3e6d6e2eb3739895b33d8ddc16
gavroshaaa <katyagavro@mail.ru>
2024-07-16 15:27:23 +0300

    исправила ошибки

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
 const sendInput = document.querySelector('#input');
-const inputForm = document.querySelector('.input-wrapper')
-const groceryContainer = document.querySelector('.groceries');
+const Item = document.querySelector('#items');
 
 sendInput.addEventListener('keydown', function(event) {
     if (event.key == 'Enter') {
         const groceriesItem = sendInput.value;
 
         const newItem = document.createElement('div');
-        newItem.classList.add('items');
         newItem.textContent = groceriesItem;
+        newItem.addEventListener('click', function() {
+            newItem.classList.toggle('done');
+        });
 
         if (groceriesItem != '') {
-            inputForm.before(newItem);
+            Item.append(newItem);
         }
 
         sendInput.value = '';
-    }
-
-    const elements = groceryContainer.querySelectorAll('.items');
-    for (let item of elements) {
-        item.addEventListener('click', function() {
-            item.classList.toggle('done');
-
-        });
     };
 });
